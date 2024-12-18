1eab8616b9e3d4b7f3a5044aa2c4c47b18d3e91c
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-12-18 12:39:42 +0300

    Adds Update role function

--- cli.py
+++ cli.py
@@ -19,3 +19,17 @@ def create_role():
     session.add(role)
     session.commit()
     print(f"Role '{name}' created with ID {role.id}")
+
+def update_role():
+    role_id = int("Enter Role ID to update: ")
+    role = session.get(Role, role_id)
+    if not role:
+        print(f"Role with ID {role_id} does not exist")
+        return
+    role.name = input(f"Enter new Role (current: {role.name}): ")
+    role.skills = input(f"Enter new skills (current: {role.skills}): ")
+    role.duties = input(f"Enter new duties (current: {role.duties}): ")
+    print(f"Role ID {role_id} updated successfully!!")
+
+def delete_role():
+    role_id = int(input("Enter Role ID to delete"))
