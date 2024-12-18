570d68b065a5c0fbf84e85a3b5eb5f3ead743e70
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-12-18 11:35:56 +0300

    Creates db

---
+++ cli.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import Base, Role, CrewMember
+
+database = "sqlite:///strawhatcrew.db"
+engine = create_engine(database)
+Session = sessionmaker(bind = engine)
+session = Session()
+
+def init_db():
--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
+
 
 class Role(Base):
     __tablename__ = 'roles'
@@ -14,19 +16,20 @@ class Role(Base):
     members = relationship('CrewMember', back_populates='role')
 
     def __repr__(self):
-        return f"Role(id = {self.id}, name= '{self.name}, skills='{self.skills}, duties='{self.duties}')"
+        return f"Role(id = {self.id}, name = '{self.name}, skills ='{self.skills}, duties ='{self.duties}')"
     
 class CrewMember(Base):
-    __tablename__ = "CrewMember"
+    __tablename__ = 'CrewMember'
 
     id = Column(Integer, primary_key=True)
     name= Column(String, nullable=False)
     ability = Column(String, nullable=False)
     dream= Column(String, nullable=False)
     bounty= Column(Integer, nullable=False)
+    role_id= Column(Integer, ForeignKey('roles.id'))
 
     role = relationship('Role', back_populates='members')
     
     def __repr__(self):
-        return f"CrewMember(id = {self.id}, name='{self.name}', ability='{self.ability}', dream='{self.dream}', bounty= {self.bounty}, role= '{self.role.name}')"
+        return f"CrewMember(id = {self.id}, name ='{self.name}', ability ='{self.ability}', dream ='{self.dream}', bounty = {self.bounty}, role_id = {self.role_id})"
     
