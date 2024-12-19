7e26eb7983b5620c6dbc034628a4db85f6c3f8c6
Debrah Navajjah Muinde <debbynav645@gmail.com>
2024-12-19 09:58:05 +0300

    Upgrades migrations

--- model.py
+++ model.py
@@ -29,7 +29,21 @@ class CrewMember(Base):
     role_id= Column(Integer, ForeignKey('roles.id'))
 
     role = relationship('Role', back_populates='members')
+    ship = relationship('Ship', back_populates='members')
     
     def __repr__(self):
         return f"CrewMember(id = {self.id}, name ='{self.name}', ability ='{self.ability}', dream ='{self.dream}', bounty = {self.bounty}, role_id = {self.role_id})"
-    
+
+class Ship(Base):
+    __tablename__ = 'ships'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    ship_wright = Column(String, nullable=False)  
+    max_capacity = Column(Integer, nullable=False)  
+
+
+    members = relationship('CrewMember', back_populates='ship') 
+
+    def __repr__(self):
+        return f"Ship(id = {self.id}, name = '{self.name}', captain = '{self.captain}', max_capacity = {self.max_capacity}, description = '{self.description}')"
